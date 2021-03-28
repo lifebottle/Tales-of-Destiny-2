cea274f4f5e60a30a3e097326f3b888f38304ce3
fortiersteven <fortiersteven1@gmail.com>
2021-03-28 10:50:10 -0400

    SLPS_Scripts update

---
+++ ps2/scripts/abcde_v0_0_9/Build_Run.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 12 21:14:08 2021
+
+@author: Steven
+"""
+import os
+import sys
+from os.path import join
+import pandas as pd
+import itertools
+import ntpath
+from tempfile import mkstemp
+from shutil import move, copymode
+from os import fdopen, remove
+import shutil
+import subprocess
+
+def replace(file_path, pattern, subst):
+    #Create temp file
+    fh, abs_path = mkstemp()
+
+    with open(file_path, "r") as old_file:
+        for line in old_file:
+            print(line)
+            if "#JMP(" in line:
+                pos = line.find(',')
+                print(line[0:(pos-1)])
+                    #new_file.write(line.replace(pattern, subst))
+    #Copy the file permissions from the old file to the new file
+    #copymode(file_path, abs_path)
+    
+    #Remove original file
+    #remove(file_path)
+    
+    #Move new file
+    #move(abs_path, file_path)
+
+def removeBlankPointerData(fileName):
+    
+    path = os.path.abspath(os.path.dirname(__file__))
+    fread = open(os.path.join(path, fileName),encoding="utf-8", mode="r")
+    fwrite = open(os.path.join(path,"w"+fileName),encoding="utf-8", mode="w")
+    
+    lines = fread.readlines()
+    indexStart = [i for i,line in enumerate(lines) if "FFFFFFFFFFF01000" in line] 
+    indexComp = [list(range(i,i+5)) for i in indexStart]
+    indexComp = list(itertools.chain.from_iterable(indexComp))
+    
+    for i,line in enumerate(lines):
+        if i not in indexComp:
+            
+            fwrite.write(line)
+            
+    fread.close()
+    fwrite.close()
+    
+   
+    shutil.copyfile( os.path.join(path,"w"+fileName), os.path.join(path,fileName))
+    remove( os.path.join(path, "w"+fileName ))
+    
+    
+def createScript(fileName, n, startPoint, step, nbObject):
+    
+    blockText = """
+#BLOCK NAME:	Items_{}
+#TYPE:		NORMAL
+#METHOD:		          POINTER_RELATIVE
+#POINTER ENDIAN:		LITTLE
+#POINTER TABLE START:	${}
+#POINTER TABLE STOP:	${}
+#POINTER SIZE:		$04
+#POINTER SPACE:		$00
+#ATLAS PTRS:		Yes
+#BASE POINTER:		$-FF000		//add $FF000 to each pointer to get
+#TABLE:			tod2_utf8.tbl	//the string address
+#COMMENTS:		No
+#END BLOCK
+"""
+
+    pathFile = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(pathFile,fileName), "w") as f:
+        f.write("#GAME NAME:			Tales of destiny 2")
+        
+        for x in range(n):
+            
+            start = hex(int(startPoint,16) + x*step)[2:].upper()
+            end   = hex(int(startPoint,16) + 4*nbObject-1 + x*step)[2:].upper()
+            f.write(blockText.format(x+1, start, end))
+    
+def runscript(file):
+    
+    args = ["perl", "abcde.pl", "-m", "bin2text", "-cm", "abcde::Cartographer", "SLPS_251.72", file+"_script.txt", file+"_dump", "-s"]
+    listFile = subprocess.run(
+        args,
+        cwd= os.path.abspath(os.path.dirname(__file__)),
+        )
+
+    
+def clean(dumpFile):
+    
+    pathFile = os.path.abspath(os.path.dirname(__file__))
+    f = open(os.path.join(pathFile,dumpFile), mode="r", encoding="utf-8")
+    destFile = open(os.path.join(pathFile,dumpFile.replace(".txt","")+"_cleaned.txt"), mode="w", encoding="utf-8")
+    for line in f:
+        if "#JMP(" not in line:
+            line = line.replace("#HDR($-FF000) // Difference between ROM and RAM addresses for pointer value calculations","")
+            line = line.replace("#ACTIVETBL(Table_0) // Activate this block's starting TABLE","")
+            line = line.replace("#W32(","#WRITE(ptr,")
+            if "//BLOCK #" in line:
+                line = ""
+            if "//POINTER " in line:
+                line = "//Text "+line.split(" ")[-1]
+                
+            #if "<$81>@" in line:
+            #    line = line.replace("<$81>@"," ")
+            destFile.write(line)
+        
+    f.close()
+    destFile.close()
+    removeBlankPointerData(dumpFile.replace(".txt","")+"_cleaned.txt")
+    os.remove( os.path.join(pathFile, dumpFile))
+    
+if __name__ == "__main__":
+    
+    #Parameters
+    fileName = sys.argv[1]
+    n = int(sys.argv[2])
+    startPoint = sys.argv[3]
+    step= int(sys.argv[4])
+    nbObject = int(sys.argv[5])
+    
+    #print("Here is an example of how to use it")
+    
+    #Create the script
+    createScript(fileName+"_script.txt", n, startPoint, step, nbObject)
+    
+    #Run the script
+    runscript(fileName)
+    
+    #Clean the dump file
+    clean(fileName+"_dump.txt")
---
+++ ps2/scripts/abcde_v0_0_9/slps_extract_item.bat
@@ -0,0 +1,2 @@
+perl abcde.pl -m bin2text -cm abcde::Cartographer "SLPS_251.72" "slps_item_script.txt" slps_item_dump -s
+pause
